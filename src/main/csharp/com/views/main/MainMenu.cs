516ec0dabbc4406b3382078a1041ab52708f8e9a
10micky10 <10miguel94@gmail.com>
2021-08-24 11:04:53 -0400

    console bank app

---
+++ src/main/csharp/com/views/main/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace App.src.main.csharp.com.views.main
+{
+    class MainMenu
+    {
+        public static void show()
+        {
+            Console.WriteLine("_____________________________");
+            Console.WriteLine("       MAIN MENU BANK        ");
+            Console.WriteLine("_____________________________");
+            Console.WriteLine("1.- User Menu");
+            Console.WriteLine("2.- Account Menu");
+            Console.WriteLine("3.- Client Menu");
+            Console.WriteLine("4.- Add Account to Client");
+            Console.WriteLine("5.- Exit");
+            Console.WriteLine("_____________________________");
+            Console.WriteLine("");
+            Console.WriteLine("Select a option");
+        }
+    }
+}
